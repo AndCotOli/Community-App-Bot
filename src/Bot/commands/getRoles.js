a1d66cbd2ead24ce429a2574a3fd4ab8fdbbd7f4
Tejas H <tejas007tez@gmail.com>
2019-08-22 17:21:49 +0530

    Minor update.

--- src/Bot/commands/getRoles.js
+++ src/Bot/commands/getRoles.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: "get-roles",
-  description: "get all availabe roles with this command",
+  description: "get all available roles with this command",
   execute(message, _args) {
     message.channel.send(`
-    Availabe roles:
+    Available roles:
 \`\`\`
   -planning
   -designer
